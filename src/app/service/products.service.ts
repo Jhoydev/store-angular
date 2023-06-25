64cee6c341f8993a98d87468d66e91a4ae39d4b8
Jhoseph Arango <jhosepharango@expivi.com>
2023-06-25 03:31:16 +0200

    use environment file - renamed methods - created getProduct method

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsService {
-  private endPoint = 'https://young-sands-07814.herokuapp.com/api';
+    private endPoint: string = environment.apiUrl;
 
-  constructor(private readonly http: HttpClient) { }
+    constructor(private readonly http: HttpClient) {
+    }
 
-  public all(): Observable<Product[]>{
-    return this.http.get<Product[]>(`${this.endPoint}/products`);
-  }
+    public getProducts(offset: number, limit: number): Observable<Product[]> {
+        return this.http.get<Product[]>(`${this.endPoint}/products`, { params: { offset,limit } });
+    }
+
+    public getProduct(aId: number): Observable<Product> {
+        return this.http.get<Product>(`${this.endPoint}/products/${aId}`);
+    }
 }
