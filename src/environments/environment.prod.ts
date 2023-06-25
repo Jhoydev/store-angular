3da062a672bccb7b0dfa74f599a4be51f0eb20c8
Jhoseph Arango <jhosepharango@expivi.com>
2023-06-25 03:30:04 +0200

    created environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://young-sands-07814.herokuapp.com/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://young-sands-07814.herokuapp.com/api'
+};
